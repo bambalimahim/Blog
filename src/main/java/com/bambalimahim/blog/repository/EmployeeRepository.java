fadeca0af73f8c57ffd3692e2135b40a72e4052d
bambamlimahim <bambamboup@esp.sn>
2018-02-23 17:03:49 +0000

    add in this project entities

---
+++ src/main/java/com/bambalimahim/blog/repository/EmployeeRepository.java
@@ -0,0 +1,16 @@
+package com.bambalimahim.blog.repository;
+
+import com.bambalimahim.blog.domain.Employee;
+import org.springframework.stereotype.Repository;
+
+import org.springframework.data.jpa.repository.*;
+
+
+/**
+ * Spring Data JPA repository for the Employee entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
